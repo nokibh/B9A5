4f8ab6ca7cc4ba9bb0b67182ef321592e1942443
nokibh <nokibhowlader2003@gmail.com>
2024-02-19 21:59:32 +0600

    added new

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        bannerImage: "url('./images/banner.png')",
+      },
+    },
+  },
+  plugins: [],
+};
